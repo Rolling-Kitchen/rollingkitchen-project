c72f00d776d189f5d8f50ec6bbd5e35bb48d08cb
Elyse <elyse-groenendaal@outlook.com>
2021-07-20 20:06:33 +0900

    added foodtruck model

---
+++ db/migrate/20210720110446_create_foodtrucks.rb
@@ -0,0 +1,14 @@
+class CreateFoodtrucks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :foodtrucks do |t|
+      t.string :name
+      t.text :description
+      t.string :food_type
+      t.text :menu_package
+      t.string :location
+      t.references :user_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
