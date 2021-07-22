2cfe354ae9b6b58a810db4c78b74d075eede331c
Riley Masunaga <rileymasunaga@gmail.com>
2021-07-22 20:50:38 +0900

    Create first version of show page and create authorization.

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -1,6 +1,11 @@
 class FoodtrucksController < ApplicationController
+
   def index
+    # Policy scope - defaults to resolve.all and shows all.
     @foodtrucks = policy_scope(Foodtruck)
   end
-  
+  def show
+    @foodtruck = Foodtruck.find(params[:id])
+    authorize @foodtruck
+  end
 end
--- app/policies/foodtruck_policy.rb
+++ app/policies/foodtruck_policy.rb
@@ -1,10 +1,14 @@
 class FoodtruckPolicy < ApplicationPolicy
+
   class Scope < Scope
     def resolve
       scope.all
     end
-    def index?
+  end
+    def show?
       return true
     end
-  end
+    def create?
+      return true
+    end
 end
