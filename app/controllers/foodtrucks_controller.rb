f7a0890ef02c758fcaa953cde858c983b9e81a61
Elyse <elyse-groenendaal@outlook.com>
2021-07-24 09:50:04 +0900

    form working, but data does not save when clicking 'submit' at the moment

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -4,8 +4,30 @@ class FoodtrucksController < ApplicationController
     # Policy scope - defaults to resolve.all and shows all.
     @foodtrucks = policy_scope(Foodtruck)
   end
+
   def show
     @foodtruck = Foodtruck.find(params[:id])
     authorize @foodtruck
   end
+  
+  def create
+    @foodtruck = Foodtruck.new(foodtruck_params)
+    if @foodtruck.save
+      redirect_to foodtruck_path(@foodtruck)
+    else
+      render :new
+    end
+    authorize @foodtruck
+  end
+
+  def new
+    @foodtruck = Foodtruck.new
+    authorize @foodtruck
+  end
+
+  private
+
+  def foodtruck_params
+    params.require(:foodtruck).permit(:name, :description, :food_type, :menu_package, :location, :photo)
+  end 
 end
--- app/policies/foodtruck_policy.rb
+++ app/policies/foodtruck_policy.rb
@@ -11,4 +11,7 @@ class FoodtruckPolicy < ApplicationPolicy
     def create?
       return true
     end
+    def new?
+      return true
+    end
 end
