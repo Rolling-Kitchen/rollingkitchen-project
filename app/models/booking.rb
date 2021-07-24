40df03765f3b9b163a24ac761605baeadae1a93e
Fauzan Zulfickry <fauzanfaturrahman@gmail.com>
2021-07-24 12:14:26 +0900

    delete bug

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :foodtruck
   belongs_to :user
 
-  validates :event_name, :event_details, :expected_attendees, :event_location presence: true
+  validates :event_name, :event_details, :expected_attendees, :event_location, presence: true
 
 
 end
