f84fb3a6830fc7cf376378b18742277041c425cc
Fauzan Zulfickry <fauzanfaturrahman@gmail.com>
2021-07-27 21:10:38 +0900

    Revert "enum"

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :foodtruck
   belongs_to :user
-  enum status: [:available, :awaiting_response, :booked]
+
   validates :event_name, :event_details, :expected_attendees, :event_location, presence: true
 
 
