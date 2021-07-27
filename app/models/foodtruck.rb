3c12dc08c2d68ae350be47c00db3dc72cd9adbda
Elyse <elyse-groenendaal@outlook.com>
2021-07-27 21:28:09 +0900

    added validation for food_price

--- app/models/foodtruck.rb
+++ app/models/foodtruck.rb
@@ -3,6 +3,6 @@ class Foodtruck < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
-  validates_presence_of :name, :description, :food_type, :menu_package, :location, on: :create, message: "can't be blank"
+  validates_presence_of :name, :description, :food_type, :food_price, :menu_package, :location, on: :create, message: "can't be blank"
 
 end
